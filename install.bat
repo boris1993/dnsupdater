9af046450d6826f2193cc2a57856a5b0cd7b3b0d
Boris Zhao <boris1993@126.com>
2018-10-13 22:05:39 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -12,35 +12,45 @@ set OPTION=%1
 if "%OPTION%" equ "-h" goto help
 if "%OPTION%" equ "--help" goto help
 if "%OPTION%" equ "/?" goto help
-goto check-arch
+goto env-prep
 
 :help
 echo Usage: install.bat TARGET
-echo TARGET could be windows-amd64, mips-softfloat
+echo TARGET could be windows-amd64, mips-softfloat, linux-amd64, darwin-amd64
 echo Default target is mips-softfloat
 goto exit
 
+:env-prep
+set OLDGOPATH=%GOPATH%
+set GOPATH=%~dp0
+goto check-arch
+
 :check-arch
 if "%1"=="windows-amd64" goto build-windows-amd64
+if "%1"=="linux-amd64" goto build-linux-amd64
+if "%1"=="darwin-amd64" goto build-darwin-amd64
 if "%1"=="mips-softfloat" goto mips-softfloat
 goto build-mips-softfloat
 
 :build-windows-amd64
-set OLDGOPATH=%GOPATH%
-set GOPATH=%~dp0
 set GOARCH=amd64
 set GOOS=windows
+goto do-build
 
+:build-linux-amd64
+set GOARCH=amd64
+set GOOS=linux
+goto do-build
+
+:build-darwin-amd64
+set GOARCH=amd64
+set GOOS=darwin
 goto do-build
 
 :build-mips-softfloat
-
-set OLDGOPATH=%GOPATH%
-set GOPATH=%~dp0
 set GOARCH=mips
 set GOOS=linux
 set GOMIPS=softfloat
-
 goto do-build
 
 :do-build
