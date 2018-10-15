e656076778467ae10899a2bb92542891c6bd9752
Boris Zhao <boris1993@126.com>
2018-10-16 01:23:06 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,64 +1,68 @@
-@echo off
+@ECHO OFF
 
-setlocal
+SETLOCAL
 
-if exist install.bat goto check-option
-echo install.bat must be run from its folder
-goto end
+IF EXIST install.bat GOTO check-option
+ECHO install.bat must be run from its folder
+GOTO end
 
 :check-option
-set OPTION=%1
+SET OPTION=%1
 
-if "%OPTION%" equ "-h" goto help
-if "%OPTION%" equ "--help" goto help
-if "%OPTION%" equ "/?" goto help
-goto :check-arch
+IF "%OPTION%" EQU "-h" GOTO help
+IF "%OPTION%" EQU "--help" GOTO help
+IF "%OPTION%" equ "/?" GOTO help
+GOTO :check-arch
 
 :help
-echo Usage: install.bat TARGET
-echo TARGET could be windows-amd64, mips-softfloat, linux-amd64, darwin-amd64
-echo Default target is mips-softfloat
-goto exit
+ECHO Usage: install.bat TARGET
+ECHO TARGET could be windows-amd64, mips-softfloat, linux-amd64, darwin-amd64
+ECHO Default target is mips-softfloat
+GOTO exit
 
 :check-arch
-if "%1"=="windows-amd64" goto build-windows-amd64
-if "%1"=="linux-amd64" goto build-linux-amd64
-if "%1"=="darwin-amd64" goto build-darwin-amd64
-if "%1"=="mips-softfloat" goto mips-softfloat
-goto build-mips-softfloat
+IF "%1"=="windows-amd64" GOTO build-windows-amd64
+IF "%1"=="linux-amd64" GOTO build-linux-amd64
+IF "%1"=="darwin-amd64" GOTO build-darwin-amd64
+IF "%1"=="mips-softfloat" GOTO mips-softfloat
+GOTO build-mips-softfloat
 
 :build-windows-amd64
-set GOARCH=amd64
-set GOOS=windows
-goto do-build
+SET GOARCH=amd64
+SET GOOS=windows
+GOTO do-build
 
 :build-linux-amd64
-set GOARCH=amd64
-set GOOS=linux
-goto do-build
+SET GOARCH=amd64
+SET GOOS=linux
+GOTO do-build
 
 :build-darwin-amd64
-set GOARCH=amd64
-set GOOS=darwin
-goto do-build
+SET GOARCH=amd64
+SET GOOS=darwin
+GOTO do-build
 
 :build-mips-softfloat
-set GOARCH=mips
-set GOOS=linux
-set GOMIPS=softfloat
-goto do-build
+SET GOARCH=mips
+SET GOOS=linux
+SET GOMIPS=softfloat
+GOTO do-build
 
 :do-build
-echo Building binary for %GOOS% running under %GOARCH%
+ECHO Building binary for %GOOS% running under %GOARCH%
 
-set APP_NAME=dnsupdater
-set PACKAGE_NAME=github.com/boris1993/%APP_NAME%
+SET APP_NAME=dnsupdater
+SET PACKAGE_NAME=github.com/boris1993/%APP_NAME%
 
-gofmt -w src
+go build -o %GOPATH%\bin\%APP_NAME% -i -v %PACKAGE_NAME%
 
-go build -o %GOPATH%/bin/%APP_NAME% -i -v %PACKAGE_NAME%
+IF errorlevel 1 GOTO error
 
-:end
-echo Build finished
+:success
+ECHO Build finished
+GOTO exit
+
+:error
+ECHO Build failed
 
 :exit
