61ca21106938571ea4d62d92d67caf846caa8f9c
Boris Zhao <boris1993@126.com>
2019-03-30 22:50:44 +0800

    Supports Aliyun DNS

---
+++ alidnsmodel/alidnsapiresponse.go
@@ -0,0 +1,31 @@
+package alidnsmodel
+
+type commonResponse struct {
+	RequestID string `json:"RequestId"`
+}
+
+// DescribeDomainRecordsResponse corresponds to the return value of the DescribeDomainRecords API
+// See document: https://help.aliyun.com/document_detail/29776.html?spm=a2c4g.11186623.2.37.1de5425696HU8m#h2-u8FD4u56DEu53C2u65703
+type DescribeDomainRecordsResponse struct {
+	commonResponse
+	TotalCount    int32           `json:"TotalCount"`
+	PageNumber    int32           `json:"PageNumber"`
+	PageSize      int32           `json:"PageSize"`
+	DomainRecords []domainRecords `json:"DomainRecords"`
+}
+
+// domainRecords describes how a DNS record is returned.
+// See document: https://help.aliyun.com/document_detail/29799.html?spm=a2c4g.11186623.2.18.37a25eb4Fu6boQ
+type domainRecords struct {
+	DomainName string
+	RecordID   string `json:"RecordId"`
+	RR         string
+	Type       string
+	Value      string
+	TTL        int32
+	Priority   int32
+	Line       string
+	Status     string
+	Locked     string
+	Weight     int32
+}
--- constants/errormsg.go
+++ constants/errormsg.go
@@ -7,9 +7,11 @@ const MsgHeaderFetchingIPOfDomain = "Fetching the IP address of domain"
 const MsgHeaderLoadingConfig = "Loading configuration from"
 const MsgHeaderDomainProcessing = "Processing"
 const MsgCloudFlareRecordsFoundSuffix = "CloudFlare DNS record(s) found."
+const MsgAliDNSRecordsFoundSuffix = "Aliyun DNS record(s) found."
 
 const MsgFormatDNSFetchResult = "The IP address of %s is %s."
-const MsgFormatUpdatingDNS = "Updating the IP address of domain %s to %s."
+const MsgFormatAliDNSFetchResult = "The IP address of %s is %s, and the record ID is %s."
+const MsgFormatUpdatingDNS = "Updating the IP address of record ID %s to %s."
 
 const ErrMsgHeaderFetchDomainInfoFailed = "Failed to get the information for domain"
 const ErrMsgHeaderUpdateDNSRecordFailed = "Failed to update the DNS record"
@@ -17,8 +19,10 @@ const MsgIPAddrNotChanged = "IP address not changed. Will not update the DNS rec
 
 const MsgCheckingCurrentIPAddr = "Checking current IP address..."
 const ErrDomainNameNotExist = "Domain name does not exist. "
+const ErrMultipleDNSRecordsFound = "Found multiple records. "
 
 const ErrCloseHTTPConnectionFail = "Error closing the HTTP connection. "
 const ErrIPAddressFetchingAPIEmpty = "API address for fetching current IP address cannot be empty. "
 const ErrCloudFlareAPIAddressEmpty = "CloudFlare API endpoint address cannot be empty. "
 const ErrCloudFlareRecordConfigIncomplete = "Incomplete CloudFlare configuration found"
+const ErrAliDNSRecordConfigIncomplete = "Incomplete Aliyun DNS configuration found"
