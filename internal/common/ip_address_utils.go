aed5a76e6e8df39624b47b7765a3efee8fb04ed1
Boris Zhao <boris1993@live.cn>
2022-09-14 16:27:02 +0800

    fix: Removing the leading zeros from the IP address parts before comparing

--- internal/common/ip_address_utils.go
+++ internal/common/ip_address_utils.go
@@ -1,15 +1,28 @@
 package common
 
-import "net"
+import (
+	"net"
+	"strings"
+)
 
 // CompareAddresses compares 2 given IP address string and see if they are the same IP address
 func CompareAddresses(address1 string, address2 string) bool {
-	ipAddr1 := net.ParseIP(address1)
-	ipAddr2 := net.ParseIP(address2)
+	ipAddr1 := net.ParseIP(removeLeadingZerosFromIPv4Address(address1))
+	ipAddr2 := net.ParseIP(removeLeadingZerosFromIPv4Address(address2))
 
 	if ipAddr1 == nil || ipAddr2 == nil {
 		return false
 	}
 
 	return ipAddr1.Equal(ipAddr2)
+}
+
+func removeLeadingZerosFromIPv4Address(ipAddress string) string {
+	ipAddressParts := strings.Split(ipAddress, ".")
+
+	for index, part := range ipAddressParts {
+		ipAddressParts[index] = strings.TrimLeft(part, "0")
+	}
+
+	return strings.Join(ipAddressParts, ".")
 }
